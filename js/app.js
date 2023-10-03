cdc8cce7429825e6fc379c71c4e96d17ac769528
jaguilar05 <jorgeaguilar05@hotmail.com>
2023-10-03 18:30:24 -0500

    añadiendo asincronismo y libreria

--- js/app.js
+++ js/app.js
@@ -1,74 +1,53 @@
-const baseDeDatos = [
-    {
-        id: 1,
-        nombre:"camiseta negra con estampado",
-        precio: 80000,
-        imagen:"../assets/img/camiseta-min.jpg"
-    },
-    {
-        id: 2,
-        nombre:"polo blanco",
-        precio: 70000,
-        imagen:"../assets/img/polo.jpg"
-    },
-    {
-        id: 3,
-        nombre:"camiseta blanca básica",
-        precio: 80000,
-        imagen:"../assets/img/camisetablanca-min.jpg"
-    },
-    {
-        id: 4,
-        nombre:"camiseta negra básica",
-        precio: 80000,
-        imagen:"../assets/img/camisetanegra-min.jpg"
-    },
-    {
-        id: 5,
-        nombre:"chaqueta negra",
-        precio: 120000,
-        imagen:"../assets/img/saconegro.jpg"
-    },
-    {
-        id: 6,
-        nombre:"polo negro",
-        precio: 80000,
-        imagen:"../assets/img/polonegro.jpg"
-    },
-    {
-        id: 7,
-        nombre:"camiseta negra básica",
-        precio: 100000,
-        imagen:"../assets/img/camisetanegra-min.jpg"
-    },
-    {
-        id: 8,
-        nombre:"camiseta blanca básica",
-        precio: 90000,
-        imagen:"../assets/img/camisetablanca-min.jpg"
-    },
-    {
-        id: 9,
-        nombre:"chaqueta blanca",
-        precio: 120000,
-        imagen:"../assets/img/busoblanco.jpg"
-    }
-]
-
-let carrito = []
+let productos = []
+async function pedirDatos(){
+    const datosSinProcesar = await fetch("../productos.json")
+    const datos = await datosSinProcesar.json()
+    productos = datos
+    
+    let carrito = []
 
 const divisa = '$'
 const DOMitems = document.querySelector('#items');
 const DOMcarrito = document.querySelector('#carrito');
 const DOMtotal = document.querySelector('#total');
 const DOMbotonVaciar = document.querySelector('#boton-vaciar');
+const DOMbotonComprar = document.querySelector('#boton-comprar')
 const miLocalStorage = window.localStorage;
 
-// Funciones
+DOMbotonComprar.addEventListener("click",()=>{
+    Swal.fire({
+        icon: 'success',
+        title: '¡Gracias por tu compra!',
+        text: 'Tu pedido llegará entre 2 a 3 días',
+        color:"black",
+        background:"white"
+      })
+      vaciarCarrito()
+})
+
+function guardarCarritoEnLocalStorage () {
+    miLocalStorage.setItem('carrito', JSON.stringify(carrito));
+}
+
+function cargarCarritoDeLocalStorage () {
+    if (miLocalStorage.getItem('carrito') !== null) {
+        carrito = JSON.parse(miLocalStorage.getItem('carrito'));
+    }
+}
+
+/**
+* Evento para añadir un producto al carrito de la compra
+*/
+function anyadirProductoAlCarrito(evento) {
+    carrito.push(evento.target.getAttribute('marcador'))
+    renderizarCarrito();
+    guardarCarritoEnLocalStorage();
+   
+}
 
 
 function renderizarProductos() {
-    baseDeDatos.forEach((info) => {
+    productos.forEach((info) => {
         const miNodo = document.createElement('div');
         miNodo.classList.add('card', 'col-sm-4');
         const miNodoCardBody = document.createElement('div');
@@ -81,10 +60,10 @@ function renderizarProductos() {
         miNodoImagen.setAttribute('src', info.imagen);
         const miNodoPrecio = document.createElement('p');
         miNodoPrecio.classList.add('card-text');
-        miNodoPrecio.textContent = `${info.precio}${divisa}`;
+        miNodoPrecio.textContent = `${divisa}${info.precio}`;
         const miNodoBoton = document.createElement('button');
         miNodoBoton.classList.add('btn', 'btn-primary');
-        miNodoBoton.textContent = 'Comprar';
+        miNodoBoton.textContent = 'Añadir al carrito';
         miNodoBoton.setAttribute('marcador', info.id);
         miNodoBoton.addEventListener('click', anyadirProductoAlCarrito);
         miNodoCardBody.appendChild(miNodoImagen);
@@ -96,41 +75,19 @@ function renderizarProductos() {
     });
 }
 
-/**
-* Evento para añadir un producto al carrito de la compra
-*/
-function anyadirProductoAlCarrito(evento) {
-    carrito.push(evento.target.getAttribute('marcador'))
-    renderizarCarrito();
-    guardarCarritoEnLocalStorage();
-}
+
 
 /**
-* Dibuja todos los productos guardados en el carrito
-*/
-function renderizarCarrito() {
-    DOMcarrito.textContent = '';
-    const carritoSinDuplicados = [...new Set(carrito)];
-    carritoSinDuplicados.forEach((item) => {
-        const miItem = baseDeDatos.filter((itemBaseDatos) => {
+ * Calcula el precio total teniendo en cuenta los productos repetidos
+ */
+
+function calcularTotal() {
+    return carrito.reduce((total, item) => {
+        const miItem = productos.filter((itemBaseDatos) => {
             return itemBaseDatos.id === parseInt(item);
         });
-        const numeroUnidadesItem = carrito.reduce((total, itemId) => {
-            return itemId === item ? total += 1 : total;
-        }, 0);
-        const miNodo = document.createElement('li');
-        miNodo.classList.add('list-group-item', 'text-right', 'mx-2');
-        miNodo.textContent = `${numeroUnidadesItem} x ${miItem[0].nombre} - ${miItem[0].precio}${divisa}`;
-        const miBoton = document.createElement('button');
-        miBoton.classList.add('btn', 'btn-danger', 'mx-5');
-        miBoton.textContent = 'X';
-        miBoton.style.marginLeft = '1rem';
-        miBoton.dataset.item = item;
-        miBoton.addEventListener('click', borrarItemCarrito);
-        miNodo.appendChild(miBoton);
-        DOMcarrito.appendChild(miNodo);
-    });
-    DOMtotal.textContent = calcularTotal();
+        return total + miItem[0].precio;
+    }, 0).toFixed(2);
 }
 
 /**
@@ -147,18 +104,6 @@ function borrarItemCarrito(evento) {
 }
 
 /**
- * Calcula el precio total teniendo en cuenta los productos repetidos
- */
-function calcularTotal() {
-    return carrito.reduce((total, item) => {
-        const miItem = baseDeDatos.filter((itemBaseDatos) => {
-            return itemBaseDatos.id === parseInt(item);
-        });
-        return total + miItem[0].precio;
-    }, 0).toFixed(2);
-}
-
-/**
 * Varia el carrito y vuelve a dibujarlo
 */
 function vaciarCarrito() {
@@ -168,15 +113,43 @@ function vaciarCarrito() {
 
 }
 
-function guardarCarritoEnLocalStorage () {
-    miLocalStorage.setItem('carrito', JSON.stringify(carrito));
-}
 
-function cargarCarritoDeLocalStorage () {
-    if (miLocalStorage.getItem('carrito') !== null) {
-        carrito = JSON.parse(miLocalStorage.getItem('carrito'));
+
+/**
+* Dibuja todos los productos guardados en el carrito
+*/
+function renderizarCarrito() {
+    DOMcarrito.textContent = '';
+    const carritoSinDuplicados = [...new Set(carrito)];
+    carritoSinDuplicados.forEach((item) => {
+        const miItem = productos.filter((itemBaseDatos) => {
+            return itemBaseDatos.id === parseInt(item);
+        });
+        const numeroUnidadesItem = carrito.reduce((total, itemId) => {
+            return itemId === item ? total += 1 : total;
+        }, 0);
+        const miNodo = document.createElement('li');
+        miNodo.classList.add('list-group-item', 'text-right', 'mx-2');
+        miNodo.textContent = `${numeroUnidadesItem} x ${miItem[0].nombre} - ${divisa}${miItem[0].precio}`;
+        const miBoton = document.createElement('button');
+        miBoton.classList.add('btn', 'btn-danger', 'mx-5');
+        miBoton.textContent = 'X';
+        miBoton.style.marginLeft = '1rem';
+        miBoton.dataset.item = item;
+        miBoton.addEventListener('click', borrarItemCarrito);
+        miNodo.appendChild(miBoton);
+        DOMcarrito.appendChild(miNodo);
+    });
+    DOMtotal.textContent = calcularTotal();
+    if(carrito.length === 0){
+        DOMbotonComprar.style.display = "none"
+    }else{
+        DOMbotonComprar.style.display = "inline"
+    
     }
 }
+
+
 
 // Eventos
 DOMbotonVaciar.addEventListener('click', vaciarCarrito);
@@ -185,3 +158,8 @@ DOMbotonVaciar.addEventListener('click', vaciarCarrito);
 cargarCarritoDeLocalStorage();
 renderizarProductos();
 renderizarCarrito();
+
+}
+
+pedirDatos()
+
